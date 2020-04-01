b48957657d78d4587ecf774e599ef9b6600c78bf
tommasogarimoldi <tommaso.garimoldi@mail.polimi.it>
2020-04-01 19:35:53 +0200

    inserimento primi metodi, beginning ancora da completare

--- src/main/java/it/polimi/ingswPSP35/client/cli/Cli.java
+++ src/main/java/it/polimi/ingswPSP35/client/cli/Cli.java
@@ -1,6 +1,6 @@
 package it.polimi.ingswPSP35.client.cli;
 
-import java.util.Scanner;
+import java.util.*;
 
 import server.model.Player;
 import server.model.Board;
@@ -12,7 +12,58 @@ import server.model.Worker;
 import server.model.Divinity;
 import server.model.ConcreteDivinity;
 
+/**
+ * This is the class used by the client for the command line interface
+ */
 public class Cli {
+
+    public Scanner input;
+
+    /**
+     * A string used for the username of the player
+     */
+    private String playername;
+
+    private int playerage;
+
+    public Cli(){
+        input = new Scanner(System.in);
+    }
+
+
+    /**
+     * Prints the welcome message
+     */
+    private void welcome(){
+        String santoriniwelcome = "Welcome To The Online Version of the board game Santorini\n" +
+                "made by Paolo Galli, Tommaso Garimoldi and Daniele Locatelli\n";
+
+        System.out.println(santoriniwelcome);
+
+    }
+
+    /**
+     * Client and player configuration
+     */
+    private void beginning(){
+        welcome();
+        System.out.println("Hello new Player, please insert your nickname:\n");
+
+        playername = input.nextLine();
+
+        System.out.println("And your age:\n");
+
+        playerage = input.nextInt();
+
+        System.out.println("Now choose a color from the List below:\n");
+     }
+
+
+
+
+
+
+
 
 
 }
