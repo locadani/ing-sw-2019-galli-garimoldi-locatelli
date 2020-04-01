b405ea1dda4177db896d9e3b6c81a282eaf0906f
tommasogarimoldi <tommaso.garimoldi@mail.polimi.it>
2020-04-01 12:48:59 +0200

    bozza cli

---
+++ src/main/java/it/polimi/ingswPSP35/client/cli/Cli.java
@@ -0,0 +1,18 @@
+package it.polimi.ingswPSP35.client.cli;
+
+import java.util.Scanner;
+
+import server.model.Player;
+import server.model.Board;
+import server.model.Cell;
+import server.model.Piece;
+import server.model.Block;
+import server.model.Dome;
+import server.model.Worker;
+import server.model.Divinity;
+import server.model.ConcreteDivinity;
+
+public class Cli {
+
+
+}
