7f904f922a55bc96f85b86cfee3c848edfece04d
tommasogarimoldi <tommaso.garimoldi@mail.polimi.it>
2020-04-06 20:02:48 +0200

    Added new metods to cli class, modified beginnig and welcome. Created class BoardPrinter

---
+++ src/main/java/it/polimi/ingswPSP35/client/cli/BoardPrinter.java
@@ -0,0 +1,12 @@
+package it.polimi.ingswPSP35.client.cli;
+
+public class BoardPrinter {
+
+    /**
+     * Prints the board
+     */
+    public static void printboard(){
+
+
+    }
+}
--- src/main/java/it/polimi/ingswPSP35/client/cli/Cli.java
+++ src/main/java/it/polimi/ingswPSP35/client/cli/Cli.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 import server.model.Player;
 import server.model.Board;
-import server.model.Cell;
+import server.model.Square;
 import server.model.Piece;
 import server.model.Block;
 import server.model.Dome;
@@ -12,12 +12,14 @@ import server.model.Worker;
 import server.model.Divinity;
 import server.model.ConcreteDivinity;
 
+
+
 /**
  * This is the class used by the client for the command line interface
  */
 public class Cli {
 
-    public Scanner input;
+    private Scanner input;
 
     /**
      * A string used for the username of the player
@@ -25,6 +27,7 @@ public class Cli {
     private String playername;
 
     private int playerage;
+
 
     public Cli(){
         input = new Scanner(System.in);
@@ -35,19 +38,54 @@ public class Cli {
      * Prints the welcome message
      */
     private void welcome(){
-        String santoriniwelcome = "Welcome To The Online Version of the board game Santorini\n" +
-                "made by Paolo Galli, Tommaso Garimoldi and Daniele Locatelli\n";
+        String santoriniwelcome = "************************************************************\n" +
+                                  "                                                             \n" +
+                                  " Welcome To The Online Version of the board game Santorini\n" +
+                                  "made by Paolo Galli, Tommaso Garimoldi and Daniele Locatelli\n" +
+                                  "                                                             \n" +
+                                  "**************************************************************\n";
 
         System.out.println(santoriniwelcome);
 
     }
 
     /**
-     * Client and player configuration
+     * Player settings for player 1 and number of players for the next game
      */
-    private void beginning(){
+    private void firstplayer(){
+
+        int numberofplayers = 0;
+
         welcome();
-        System.out.println("Hello new Player, please insert your nickname:\n");
+        System.out.println("Hello Player 1, please enter a nickname:\n");
+
+        playername = input.nextLine();
+
+        System.out.println("Type 2 if you want to play a two players match or 3 if you want to play a three players match:\n");
+
+        numberofplayers = input.nextInt();
+
+        while (numberofplayers < 2 || numberofplayers > 3){
+
+            System.out.println("Format not valid please type 2 for a two players match or 3 for a three players match");
+            numberofplayers = input.nextInt();
+        }
+
+
+        System.out.println("Now insert your age:\n");
+
+        playerage = input.nextInt();
+
+
+
+    }
+
+    /**
+     * Player settings for second and third players
+     */
+    private void genericplayer(){
+        welcome();
+        System.out.println("Hello new Player, please enter a nickname:\n");
 
         playername = input.nextLine();
 
@@ -59,11 +97,65 @@ public class Cli {
      }
 
 
+    /**
+     * Connection configuration
+     */
+     private void onlineconfig(){
+
+         boolean connected = false;
+
+         //tryconnection();
+
+         if (connected = true)
+             System.out.println("Connection Established!");
+          else {
+              System.out.println("Something went wrong connection not established; please try again.");
+              onlineconfig();
+          }
+
+
+
+         }
+
+    /**
+     * Manages a player's turn
+     */
+      private void turn(){
+          boolean myturn = true;
+          int action;
+
+          while (myturn = true) {
+              System.out.println("It's your turn");
+
+
+              BoardPrinter.printboard();
+
+              System.out.println("Choose between moving(0) or using your divinity power(1) if possible:\n");
+
+              action = input.nextInt();
+              while (action < 0 || action > 1){
+
+                  System.out.println("Format not valid please type 0 to move or 1 to use your divinity power if possible:\n");
+                  action = input.nextInt();
+              }
+
+              System.out.println("Choose a worker to move:\n");
+
+              System.out.println("Choose a cell:\n");
+
+              System.out.println("Where do you want to build?");
 
 
 
 
 
 
+          }
 
-}
+      }
+
+
+
+
+
+     }
