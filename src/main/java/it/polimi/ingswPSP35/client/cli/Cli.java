bf2660dc081ea27135d9724a34c3a6551fdb4a21
tommasogarimoldi <tommaso.garimoldi@mail.polimi.it>
2020-04-28 16:39:07 +0200

    Cli methods modified deleted Printer Class

--- src/main/java/it/polimi/ingswPSP35/client/cli/Cli.java
+++ src/main/java/it/polimi/ingswPSP35/client/cli/Cli.java
@@ -6,7 +6,7 @@ import java.util.*;
 /**
  * This is the class used by the client for the command line interface
  */
-public class Cli {
+public class Cli implements UInterface{
 
     private Scanner input;
 
@@ -38,7 +38,7 @@ public class Cli {
 
     /**
      * Player settings for player 1 and number of players for the next game
-     * @return
+     * @return number of players
      */
     public int getNPlayers() {
 
@@ -59,6 +59,11 @@ public class Cli {
         return numberofplayers;
     }
 
+    /**
+     * Asks the first player to choose the divinites for the game
+     * @param numberofplayers
+     * @return list of divinities choosen by the first player
+     */
     public List<String> getDivinities(int numberofplayers){
 
         int value;
@@ -83,9 +88,9 @@ public class Cli {
 
     /**
      * Player settings for second and third players
-     * @return
+     * @return players info
      */
-    public String[] getplayerinfo(){
+    public String[] getPlayerInfo(){
         welcome();
         String[] playerinfo = new String[2];
 
@@ -105,24 +110,45 @@ public class Cli {
 
     /**
      * returns to the player his divinity and asks the player to place his workers on the board
+     * @return the divinity choosen by the player
      */
-     public void divinityworkerplacer(){
+     public String chooseDivinity(List<String> divinitiesList) {
 
-        int cell0, cell1;
+         int value = 0;
 
-         System.out.println("this is your divinity:\n");
+         System.out.println("Choose your divinity:\n");
 
-         //choosedivinity();
+         for (int i = 0; i < divinitiesList.size(); i++) {
+             System.out.println(i + ": " + divinitiesList.get(i));
+         }
 
-         Printer.printboard();
+         while (value >= divinitiesList.size()) {
+
+             value = input.nextInt();
+
+         }
+
+         return divinitiesList.get(value);
+     }
+
+
+    /**
+     * Asks the player to place the workers at the beginning of the game
+     * @return
+     */
+    public int getPosition(){
+        int cell;
+
+         //Printer.printboard();
+
          System.out.println("select the cell for the first worker:\n");
 
-         cell0 = input.nextInt();
+         cell = input.nextInt();
 
-         System.out.println("select the cell for the second worker:\n");
+         return cell;
 
-         cell1 = input.nextInt();
     }
+
 
 
     /**
@@ -141,24 +167,24 @@ public class Cli {
      * Manages a player's turn
      * @return boolean my turn for Client class
      */
-    public boolean turn(boolean myturn){
-          String action;
-          int workernumber, cell;
+    public String performAction(){
+
+        String requestedAction = null;
+        int action, workernumber, cell;
 
           System.out.println("It's your turn");
 
-              Printer.printboard();
+              //Printer.printboard();
 
               System.out.println("Choose an action to do:\n");
 
               getactionslist();
 
-              action = input.nextLine();
+              action = input.nextInt();
 
-              while (action != "endturn") {
-                  switch (action) {
+              switch (action) {
 
-                      case "move": {
+                      case 0:
                           System.out.println("Choose a worker to move:\n");
 
                           workernumber = input.nextInt();
@@ -167,49 +193,45 @@ public class Cli {
 
                           cell = input.nextInt();
 
-                          System.out.println("Choose an action to do:\n");
+                          requestedAction = workernumber + "|move|" + cell;
+                      break;
 
-                          Printer.printboard();
+                      case 1:
+                          System.out.println("Choose a worker to move:\n");
 
-                          getactionslist();
+                          workernumber = input.nextInt();
 
-                          action = input.nextLine();
-                      }
+                          System.out.println("Choose a cell:\n");
 
-                      case "build": {
-                          System.out.println("In which cell do you want to build?\n");
-
-                          Printer.printboard();
                           cell = input.nextInt();
 
-                          System.out.println("Choose an action to do:\n");
+                          requestedAction = workernumber + "|build|" + cell;
+                          break;
 
-                          getactionslist();
 
-                          action = input.nextLine();
-                      }
-
-                      case "godpower": {
+                      case 2: {
 
                           System.out.println("Choose an action to do:\n");
 
                           getactionslist();
 
-                          action = input.nextLine();
-                      }
+                          action = input.nextInt();
+                      } break;
 
-                      case "endturn":{
-                          myturn = false;
-                          waitforyourturn();
-                      }
-                  }
+                      case 3:
+                          requestedAction = "endturn";
+
+
+
               }
-              return myturn;
 
+         return requestedAction;
     }
 
 
-
+    /**
+     * prints the waiting line
+     */
     private void waitforyourturn(){
 
         System.out.println("waiting for your turn to start....");
@@ -221,20 +243,26 @@ public class Cli {
      */
     private void getactionslist(){
 
-          String actionslist = "move\n"+
-                               "build\n"+
-                               "godpower\n"+
-                               "endturn\n";
+          String[] actionslist = {"move", "build", "godpower", "endturn"};
 
-        System.out.println(actionslist);
+        for (int i = 0; i < actionslist.length; i++) {
+            System.out.println(i + ": " + actionslist[i]);
+        }
+
       }
 
+    /**
+     * Prints the win message
+     */
     public void win(){
         String victory = "You won this game congratulations!\n";
 
         System.out.println(victory);
     }
 
+    /**
+     * Prints the loss message
+     */
     public void loss(){
         String loss = "You Lost";
 
